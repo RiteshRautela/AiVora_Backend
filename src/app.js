131e2baca40b2bfecad40d13827f4846696bb0c5
Riteshrautela2556 <riteshrautela750@gmail.com>
2026-03-28 00:21:56 +0530

    Add CORS headers for cookie requests

--- src/app.js
+++ src/app.js
@@ -2,25 +2,34 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
-
-const app = express();   
-const cors = require("cors");
-const PORT = process.env.PORT || 7777;
-
-
-app.use(express.json()); 
-app.use(cookieParser());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true, 
-   methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
-}))
-
-
 const authRouter = require("./routes/auth")
 const userRouter  = require("./routes/user")
 const websiteRouter = require("./routes/website")
 const paymentRouter = require("./routes/payment")
+const app = express();   
+const cors = require("cors");
+const PORT = process.env.PORT || 7777;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json()); 
+app.use(cookieParser());
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+//   credentials: true, 
+//    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+// }))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
+
+
 
 app.use("/" , authRouter)
 app.use("/" , userRouter)
